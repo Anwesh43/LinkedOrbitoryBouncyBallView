e8437f5493646c18b0ccb9ee135058e15f516f03
Anwesh43 <anweshthecool0@gmail.com>
2020-02-26 09:59:06 +0530

    adding constants and libraries

--- orbitorybouncyballview/src/main/java/com/anwesh/uiprojects/orbitorybouncyballview/OrbitoryBouncyBallView.kt
+++ orbitorybouncyballview/src/main/java/com/anwesh/uiprojects/orbitorybouncyballview/OrbitoryBouncyBallView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.orbitorybouncyballview
 /**
  * Created by anweshmishra on 26/02/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val scGap : Float = 0.02f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#3F51B5")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20

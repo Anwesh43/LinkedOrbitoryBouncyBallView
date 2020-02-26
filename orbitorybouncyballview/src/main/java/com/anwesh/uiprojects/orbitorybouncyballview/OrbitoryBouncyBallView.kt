65cb06e2ef5c2e5b2061f14cd075205352294775
Anwesh43 <anweshthecool0@gmail.com>
2020-02-26 10:24:16 +0530

    adding OBBNode class

--- orbitorybouncyballview/src/main/java/com/anwesh/uiprojects/orbitorybouncyballview/OrbitoryBouncyBallView.kt
+++ orbitorybouncyballview/src/main/java/com/anwesh/uiprojects/orbitorybouncyballview/OrbitoryBouncyBallView.kt
@@ -117,4 +117,46 @@ class OrbitoryBouncyBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class OBBNode(var i : Int, val state : State = State()) {
+
+        private var next : OBBNode? = null
+        private var prev : OBBNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = OBBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawOBBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : OBBNode {
+            var curr : OBBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
